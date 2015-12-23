be5a67dea2a4d336cf6ac5ff767c06045b68b3b4
Tobi Lehman <tobi.lehman@gmail.com>
2015-12-22 22:52:48 -0800

    Google Vision API request/response working

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
 OCR is short for Optical Character Recognition. Recently, Google Released an alpha version of their Vision API, which uses deep neural networks to classify image data, OCR is one of the services the Google Vision API offers. This site aims to make their OCR services easier to use.
 
+TO-DO:
+  - Store image hash to response so as to avoid wasting API calls.
+
 
--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 )
+
+const googleVisionApiUrl = "https://vision.googleapis.com/v1alpha1/images:annotate?key="
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	buf, _ := ioutil.ReadFile("index.html")
@@ -13,7 +17,16 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 
 func recognizeHandler(w http.ResponseWriter, r *http.Request) {
 	body, _ := ioutil.ReadAll(r.Body)
-	fmt.Println(string(body))
+	url := googleVisionApiUrl + os.Getenv("GVAPIKEY")
+	req, _ := http.NewRequest("POST", url, bytes.NewBuffer(body))
+	req.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Printf("request error: %v", err)
+	}
+	resbody, _ := ioutil.ReadAll(res.Body)
+	fmt.Fprintf(w, "%s", resbody)
 }
 
 func main() {
